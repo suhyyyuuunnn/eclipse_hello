c179faf7b9f8e8a3b102780f760838ed42b90bce
이수현 <@DESKTOP-JBT5K3G>
2019-04-26 14:17:14 +0900

    afa

--- Hello_world/src/HelloWorld.java
+++ Hello_world/src/HelloWorld.java
@@ -2,7 +2,9 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("java");
+		System.out.println("git");
+		System.out.println("zdz");
 
 	}
 
